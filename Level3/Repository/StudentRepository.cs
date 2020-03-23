e244e26b593bd158da693b9dcc4d9d8b10953e6e
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 16:54:30 +0530

    code Added

---
+++ Level3/Repository/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Level3.Repository
+{
+    public class StudentRepository
+    {
+        public void Get()
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+    }
+
+}
